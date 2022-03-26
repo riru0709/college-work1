464b6cfae3c4b31d608f900fa4685eda5979eca8
riru0709 <riruprrzm1121@gmail.com>
2022-03-26 23:30:43 +0900

    8-2課題2まで

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,11 +8,13 @@
     </head>
     <body>
         <h1>Blog-title</h1>
-        <div class='posts'>
-            <div class='post'>
-                <h2 class='title'>post-title1</h2>
-                <p class='body'>This is a sample body.</p>
-            </div>
+	<div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
         </div>
     </body>
 </html>
