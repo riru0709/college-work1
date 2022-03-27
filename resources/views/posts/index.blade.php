5d824c1914fbc2a2d11d956a6cc04f940fcb7ae4
riru0709 <riruprrzm1121@gmail.com>
2022-03-27 11:13:00 +0900

    ページネーション10件分

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,13 +8,16 @@
     </head>
     <body>
         <h1>Blog-title</h1>
-	<div class='posts'>
+	    <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
                     <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
