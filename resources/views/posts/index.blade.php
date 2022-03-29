3fa1e2a880463f444dfd2f1d611d5d9c8be847fc
riru0709 <riruprrzm1121@gmail.com>
2022-03-29 21:47:47 +0900

    8-3完了

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,14 +4,16 @@
         <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog-title</h1>
-	    <div class='posts'>
+        <h1>BlogName</h1>
+        <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+　　　　　　　　　　</h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
