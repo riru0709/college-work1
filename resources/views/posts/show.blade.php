4dda278d1a3f76bb0383442789378d53e2d4ef2d
riru0709 <riruprrzm1121@gmail.com>
2022-03-31 15:51:39 +0900

    8-6完了

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,11 +9,16 @@
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
+        <h1 class="title">BlogName</h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+            @csrf
+            @method('DELETE')
+            <input type="submit" style="display:none">
+            <p class='delete'>[<span onclick="return deletePost(this);">削除</span>]</p>
+        </form>
 	    <div class='content'>
+	        <div class="content_title">{{ $post->title }}</div>
             <div class="content_post">
                 <h3>本文</h3>
                 <p>{{ $post->body }}</p>
@@ -22,5 +27,13 @@
         <div class='footer'>
             <a href="/">戻る</a>
         </div>
+        <script>
+            function deletePost(e){
+                'use strict';
+                if(confirm('本当に削除しますか？')){
+                    document.getElementById('form_{{ $post->id }}').submit();
+                }
+            }
+        </script>
     </body>
 </html>
