2948d47fa43cd28b2206493b1276173dfb59e088
riru0709 <riruprrzm1121@gmail.com>
2022-03-31 14:06:23 +0900

    8-4完了

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -10,14 +10,16 @@
             @csrf
             <div class="title">
                 <h2>タイトル</h2>
-                <input type="text" name="post[title]" placeholder="タイトル" />
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>本文</h2>
-                <textarea name="post[body]" placeholder="今日のおやつ"></textarea>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ old('post.body') }}</textarea>
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
-            <input type="submit" value="保存"></input>
+            <input type="submit" value="保存"/>
         </form>
-        <div class="back">[<a href="/">back</a>]</div>
+        <div class="back">[<a href="/">戻る</a>]</div>
     </body>
 </html>
